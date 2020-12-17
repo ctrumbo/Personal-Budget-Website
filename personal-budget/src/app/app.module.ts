348aff5c3fc1b28edec4fbd4b277750121f61817
Your name here <ctrumbo@uncc.edu>
2020-12-16 19:35:45 -0500

    added components

--- personal-budget/src/app/app.module.ts
+++ personal-budget/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AccountComponent } from './account/account.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    DashboardComponent,
+    AccountComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
